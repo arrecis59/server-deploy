7c9f8ef6a8b2b869b485113621e982766296694d
em-Qwerty <arrecis59@gmail.com>
2019-10-27 03:39:19 -0600

    correccion ortografia derechos

--- routes/initial_routes.js
+++ routes/initial_routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ "message": "Bienvenido a la api (detección de enfermedades cutáneas )", "desarrollador": "José Mario Arrecis monterroso", "año": 2019 });
+    res.status(200).json({ "message": "Bienvenido a la api (detección de enfermedades cutáneas )", "desarrollador": "José Mario Arrecis Monterroso", "año": 2019 });
 });
 
 module.exports = router;
